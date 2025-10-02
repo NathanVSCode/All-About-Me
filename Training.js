99a97af0d4e24fbf632f39202f7997fb8ab766ad
NathanVSCode <158079246+NathanVSCode@users.noreply.github.com>
2025-10-02 15:57:58 -0400

    Add files via upload

---
+++ Training.js
@@ -0,0 +1,163 @@
+//  Functions Decloration
+//  function Nathan(){
+//      console.log('This is a coooooool kid')
+//      }
+    
+//      Nathan();
+//      // returning values
+//      const calcArea = function(radius){
+//              return 3.14*radius**2;
+//         ;
+//      }
+//      const area = calcArea(102);
+//      console.log(area)
+    
+//     //arrow function
+//     const calcarea = radius => {
+//         return 3.14*radius**2;
+//     }
+    
+//      //functions
+//      const greet = () => 'hello;'
+    
+//      let resultOne = greet();
+//      //methods
+    
+//      let resultTwo = name.toUpperCase();
+//      console.log(resultTwo)
+//      callbacks
+//  const MyFunc = (callbackFunk) => {
+//     let value = 100
+//  }
+//      let value = 100
+//      console.log(value);
+//      //foreach
+//      let people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li']
+     
+//      people.forEach(function(person) {
+//      });
+//      const logPerson (person,index) => {
+//         console.log(`${index} - hello ${person}`);
+//      };
+    //  const adding = function(Number1,Number2){
+    //     return Number1+Number2;
+    // }
+    
+    // console.log(adding(50, 20));
+
+    // object literals
+    //  let user = {
+    //     name: 'Nathan',
+    //     age: 12,
+    //     email: 'nathanhenok1010@gmail.com',
+    //     location: 'Silver Spring, Maryland',
+    //     my_favorite_team(){
+    //         console.log('My favorite soccer team is Chelsea FC! GO BLUES')
+    //     }
+    //     };
+        
+        // console.log(user.name)
+        // console.log(user['location'])
+
+        // Math Objects
+
+        // console.log(Math);
+        // console.log(Math.PI);
+        // console.log(Math.E);
+
+        // const number = 100.4
+        // console.log(Math.round(area));
+        // console.log(Math.floor(area));
+        // console.log(Math.ceil(area));
+        // console.log(Math.trunc(area));
+
+        // random numbers
+
+        // const random = Math.random();
+
+        // console.log(Math.round(random * 100))
+
+        // The Document object Model(DOM)
+
+        // const para = document.querySelector('p');
+
+        // console.log(para);
+
+        // Get an element by it's ID
+
+        // const title = document.getElementById(/* ID you want to grab. Ex 'title-name' */ ) 
+
+        // console.log(title)
+
+        // Get elements by their class name
+        // const errors = document.getElementsByClassName(/* Class you want to grab. Ex 'error' */)
+        // console.log(errors)
+        // console.log(errors[0])
+        // Get elements by their tag name
+        // const paras = document.getElementsByTagName(/* Tag you want to grab. Ex 'p' */)
+        // console.log(paras)
+        // console.log(paras[0])
+
+
+        // How to override text in JavaScript
+
+        // const para = document.querySelector('p');
+
+        // para.innerText = 'NATHAN IS THE BEST!'; 
+        /*innerText gets you the property of the element */
+
+        // How to change the text of many items at once.
+
+        // const paras = document.querySelectorAll('p');
+
+        // paras.forEach(para => {
+        //     console.log(para.innerText);
+        //     para.innerText += ' new text';
+        // });
+ // How to add and remove classes from elements.
+//  const  content = document.querySelector('p');
+
+//  console.log(content.classList);
+//  content.classList.add('error');
+
+//  how to get rid of a classes
+//  content.classList.remove('error');
+
+// 'This' Keyword  
+
+// The 'This' Keyword is an abstract way to show the object that 
+// owns it and the code that is writen in the object
+
+
+// The date object
+
+// The my date object could be used to show the date and compare dates. You can also use it to show if it's the
+// morning,Afternoon,Evening,or night using if statements. It it inbuilt in Javascript. The way you can store 
+// it is by making it a variable like this. "var myDate = new Date" Then you can play around with it using 
+// conditionals to make it usefull in a project.
+
+
+// Text content. 
+
+// It is something that you can use to change the content of a string
+
+// let title = document.getElementById("title");
+// title.textContent
+// title.textContent = "Roll Tide!"
+
+// Events!
+
+// Events are used to make your webpage do things when you interact with it.
+// onclick,onMouseOver are common to use when using events in Javascript.
+//Using functions with events is common.
+
+// title.onclick = function(){
+//    alert('Hello')
+// }
+
+// DOM
+
+// The DOM is used to change elements in an html page by using things like getElementByID and getElementsByTagName.
+
+// Using InnerHtml, you can change tags. Using TextContent, you can change the text the tag shows.
+
